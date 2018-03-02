a7fe430dc01f2150fd1c9265542b78d5585a2b8c
Salomon ROSSELL <salomon.rossellpro@gmail.com>
2018-03-02 23:14:13 +0100

    [INIT] premier commit

---
+++ src/components/app-home/app-home.tsx
@@ -0,0 +1,36 @@
+import { Component } from '@stencil/core';
+
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.scss'
+})
+export class AppHome {
+
+  render() {
+    return (
+      <ion-page class='show-page'>
+        <ion-header md-height='56px'>
+          <ion-toolbar class="toolbar" color='primary'>
+            <ion-title>La Mobile Factory by SopraSteria</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+          <p>
+            Bienvenue sur l'application (Progressive Web App) de La Mobile Factory by SopraSteria !
+          </p>
+          <p>
+            Retrouvez ici tout ce que vous devez savoir sur nous !
+          </p>
+
+          <stencil-route-link url='/profile/stencil'>
+            <ion-button>
+              Profile page
+            </ion-button>
+          </stencil-route-link>
+        </ion-content>
+      </ion-page>
+    );
+  }
+}
---
+++ stencil.config.js
@@ -0,0 +1,15 @@
+exports.config = {
+  collections: [
+    { name: '@stencil/router' },
+    { name: '@ionic/core' }
+  ],
+  serviceWorker: {
+    swSrc: 'src/sw.js'
+  },
+  globalStyle: 'src/global/app.css'
+};
+
+exports.devServer = {
+  root: 'www',
+  watchGlob: '**/**'
+};
