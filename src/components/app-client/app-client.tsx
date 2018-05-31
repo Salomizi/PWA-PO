ffb358b6a75a6d1c413b8dd09d89056c0514b8ec
RODRIGUEZ Guillaume <guillaumerodriguez@free.fr>
2018-05-31 17:44:29 +0200

    UPDATE : ESSAI de Changement de swiper

--- src/components/app-client/app-client.tsx
+++ src/components/app-client/app-client.tsx
@@ -10,7 +10,6 @@ import { Projet } from '../../model/projet';
 export class AppClient {
 
     @State() element;
-
     @State() rootPath: string;
     @State() fileExtension: string;
 
@@ -18,36 +17,39 @@ export class AppClient {
 
     @State() pictures: string[];
 
-    // @State() parentSwiper: any;
+
+
+    swiperH: Swiper;
+    swiperV: Swiper;
 
     componentWillLoad() {
         this.fileExtension = getFilesExtension();
         this.rootPath = getProjectsRootPath();
         this.projets = getProjets();
 
-
-        // this.parentSwiper = document.getElementById('parentSlider');
-
-        // if (this.parentSwiper != null) {
-        //     console.log('parentSwiper has been found');
-        // }
+        this.swiperH = new Swiper('.swiper-container-h', {
+            spaceBetween: 50,
+            pagination: {
+                el: '.swiper-pagination-h',
+                clickable: true,
+            },
+        });
+        this.swiperV = new Swiper('.swiper-container-v', {
+            direction: 'horizontal',
+            spaceBetween: 50,
+            pagination: {
+                el: '.swiper-pagination-v',
+                clickable: true,
+            },
+            nested:true
+        });
     }
 
     componentDidLoad() {
         console.log('The component has been rendered');
     }
 
-    // swiperSlideChanged(event: UIEvent) {
-    //     if (event) return;
-    //     let currentIndex = this.parentSwiper.getActiveIndex();
-    //     console.log('swipersSlideChanged hit');
-    //     if (!this.parentSwiper.isEnd()) {
-    //         console.log('slideto' + (currentIndex + 1));
-    //         this.parentSwiper.slideTo(currentIndex + 1);
-    //     }
 
-
-    // }
 
     render() {
 
@@ -55,7 +57,7 @@ export class AppClient {
             <ion-page>
                 <ion-content>
                     <app-title />
-                    <ion-slides pager id='parentSlider'>
+                    {/* <ion-slides pager id='parentSlider'>
                         {this.projets.map((projet) => {
                             return <ion-slide class='swiper-slide'>
                                 <div class='verticalDiv'>
@@ -74,7 +76,33 @@ export class AppClient {
                                 </div>
                             </ion-slide>
                         })}
-                    </ion-slides>
+                    </ion-slides> */}
+
+                    <div class="swiper-container swiper-container-h">
+                        <div class="swiper-wrapper">
+                            {this.projets.map((projet) => {
+                                return <div class="swiper-slide">
+                                    <div class='verticalDiv'>
+                                        <div class='flexDiv'>
+                                            <img class='logo' src={this.rootPath + projet.directory_name + 'logo' + this.fileExtension} />
+                                            <div class='textDiv'>{projet.description}</div>
+                                        </div>
+                                        <div class="swiper-container swiper-container-v">
+                                            <div class="swiper-wrapper">
+                                                {projet.screenshots.map((picture) => {
+                                                    return <div class="swiper-slide">
+                                                        <lazy-img src={this.rootPath + projet.directory_name + picture + this.fileExtension} />
+                                                    </div>
+                                                })}
+                                            </div>
+                                            <div class="swiper-pagination swiper-pagination-v"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            })}
+                        </div>
+                        <div class="swiper-pagination swiper-pagination-h"></div>
+                    </div>
                 </ion-content>
             </ion-page>
         );
