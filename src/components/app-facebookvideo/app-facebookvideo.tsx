5bd6f88c21a3079d5e4035ed254a77913915af0e
RODRIGUEZ Guillaume <guillaume.rodriguez@soprasteria.com>
2018-07-06 18:45:53 +0200

    FIX : Problème de taille du composant facebook

--- src/components/app-facebookvideo/app-facebookvideo.tsx
+++ src/components/app-facebookvideo/app-facebookvideo.tsx
@@ -1,18 +1,32 @@
-import { Component } from '@stencil/core';
+import { Component, Listen, State } from '@stencil/core';
 
 @Component({
   tag: 'app-facebookvideo',
   styleUrl: 'app-facebookvideo.scss'
 })
 export class AppFacebookVideo {
+  @State() componentHeight: number;
+  @Listen('window:resize')
+  windowResize() {
+    this.changeComponentHeight();
+  }
 
+  componentDidLoad() {
+    this.changeComponentHeight();
+  }
 
+  private changeComponentHeight() {
+
+    let width = window.innerWidth;
+    console.log(width);
+    this.componentHeight = width / 1.75;
+  }
   render() {
 
     return (
 
       <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fsoprasteria%2Fvideos%2F1758810114153796%2F" frameborder="0" scrolling="no" width="100%" ></iframe>
+        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fsoprasteria%2Fvideos%2F1758810114153796%2F" frameborder="0" scrolling="no" width="100%" height={this.componentHeight + 'px'}></iframe >
 
     );
   }
